47af4192f982fb9aa436ca842a97c85475dbb72e
DevanshuThaker <utaker834@gmail.com>
2026-07-04 17:36:27 +0530

    Day 2: JS Functions - regular, arrow, default params

---
+++ 02-functions.js
@@ -0,0 +1,22 @@
+﻿// Day 2 - Functions
+
+// Regular function
+function calculateTotal(price, qty) {
+  return price * qty;
+}
+
+// Arrow function
+const calculateTotalArrow = (price, qty) => price * qty;
+
+// Default parameters
+function applyDiscount(price, discount = 0) {
+  return price - discount;
+}
+
+// Function as a value
+const operation = calculateTotal;
+console.log(operation(100, 2)); // 200
+
+console.log(calculateTotal(220, 2));      // 440
+console.log(calculateTotalArrow(220, 2)); // 440
+console.log(applyDiscount(220));          // 220
