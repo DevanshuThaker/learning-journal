e978c3f05fb4ffce058e32893f807aa9a0288d05
DevanshuThaker <utaker834@gmail.com>
2026-07-04 17:37:13 +0530

    Day 4: JS Objects - creation, access, nesting, spread copy

---
+++ 04-objects.js
@@ -0,0 +1,31 @@
+﻿// Day 4 - Objects
+
+const menuItem = {
+  name: "Paneer Butter Masala",
+  price: 220,
+  isVeg: true
+};
+
+console.log(menuItem.name, menuItem.price);
+
+menuItem.price = 240; // modifying
+console.log(menuItem.price);
+
+const order = {
+  id: 101,
+  customer: {
+    name: "Devanshu",
+    address: "Gandhinagar"
+  }
+};
+console.log(order.customer.name);
+
+const menuList = [
+  { name: "Paneer Tikka", price: 180 },
+  { name: "Veg Biryani", price: 200 }
+];
+console.log(menuList[0].name);
+
+// Copying with spread
+const updatedItem = { ...menuItem, price: 250 };
+console.log(updatedItem);
