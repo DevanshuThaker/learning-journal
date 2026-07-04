19b5a38d409ea31561bec60e1fd5d14797c55297
DevanshuThaker <utaker834@gmail.com>
2026-07-04 17:36:09 +0530

    Day 1: JS Variables & Data Types

---
+++ 01-variables-datatypes.js
@@ -0,0 +1,19 @@
+﻿// Day 1 - Variables & Data Types
+
+let orderCount = 5;              // number
+const restaurantName = "Spice Villa"; // string
+let isVeg = true;                // boolean
+let discount = null;             // null
+let deliveryTime;                // undefined
+
+const menuItems = ["Paneer Tikka", "Naan", "Dal Makhani"]; // array
+
+const cart = {
+  restaurantName: "Spice Villa",
+  totalAmount: 450,
+  isVeg: true
+}; // object
+
+console.log(orderCount, restaurantName, isVeg, discount, deliveryTime);
+console.log(menuItems);
+console.log(cart);
