a7832e211277ff3a38231a1fa29e295c450a84e1
DevanshuThaker <utaker834@gmail.com>
2026-07-04 17:36:46 +0530

    Day 3: JS Arrays - forEach, map, filter, find, reduce

---
+++ 03-arrays-and-methods.js
@@ -0,0 +1,17 @@
+﻿// Day 3 - Arrays & Array Methods
+
+const prices = [220, 150, 80, 300];
+
+prices.forEach(p => console.log(p));
+
+const withTax = prices.map(p => p * 1.05);
+console.log(withTax);
+
+const affordable = prices.filter(p => p < 200);
+console.log(affordable);
+
+const found = prices.find(p => p > 250);
+console.log(found);
+
+const total = prices.reduce((sum, p) => sum + p, 0);
+console.log(total);
